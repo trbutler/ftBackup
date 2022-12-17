a0308944e0a0f8f0c5041a11fe18c31747465d08
timothy butler <timothybutler@timothys-MacBook-Pro.local>
2022-12-17 15:47:44 -0600

    Adjusted permissions

--- installer.sh
+++ installer.sh
@@ -6,7 +6,7 @@ cp FaithTree/Backup.pm /var/cpanel/perl5/lib/FaithTree/
 cp FaithTree/Backup/Logger.pm /var/cpanel/perl5/lib/FaithTree/Backup/
 cp ftBackup.pl /var/cpanel/perl5/lib/
 ln -s /var/cpanel/perl5/lib/ftBackup.pl /usr/local/bin/ftBackup 
-chmod 700 /var/cpanel/perl5/lib/ftBackup.pl
+chmod 755 /var/cpanel/perl5/lib/ftBackup.pl
 cpan Net::Amazon::S3
 cpan Term::ProgressBar
 /usr/local/cpanel/bin/manage_hooks add script /var/cpanel/perl5/lib/ftBackup.pl --manual --category=System --event=Backup --stage=post
