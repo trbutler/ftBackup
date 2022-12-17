40dac4bd7b67d741ec97afde94232e1aa686cc94
timothy butler <timothybutler@timothys-MacBook-Pro.local>
2022-12-17 15:36:49 -0600

    Improved installer

--- installer.sh
+++ installer.sh
@@ -4,6 +4,9 @@ mkdir /var/cpanel/perl5/lib/FaithTree
 mkdir /var/cpanel/perl5/lib/FaithTree/Backup
 cp FaithTree/Backup.pm /var/cpanel/perl5/lib/FaithTree/
 cp FaithTree/Backup/Logger.pm /var/cpanel/perl5/lib/FaithTree/Backup/
+cp ftBackup.pl /var/cpanel/perl5/lib/
+ln -s /usr/local/bin/ftBackup /var/cpanel/perl5/lib/ftBackup.pl
+chmod 700 /usr/local/bin/ftBackup
 cpan Net::Amazon::S3
 cpan Term::ProgressBar
 /usr/local/cpanel/bin/manage_hooks add module FaithTree::Backup
