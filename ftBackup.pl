0bdd974d20c24f664d84ac175cb3e24664be982a
timothy butler <timothybutler@timothys-MacBook-Pro.local>
2022-12-17 11:56:31 -0600

    Working version

--- ftBackup.pl
+++ ftBackup.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/local/cpanel/3rdparty/bin/perl
 # FaithTree.com cPanel Incremental to S3 Backup Tool
 # 
 # A tool to take daily incremental backups from cPanel, upload them to S3 and rotate the copies on S3.
@@ -10,7 +10,7 @@
 # 	under the terms of the GNU General Public License as published by the
 # 	Free Software Foundation, either version 2 of the License, or (at your
 # 	option) any later version. This program is distributed in the hope that
-# 	it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# 	it will be useful, but WITHOUT ANY WARRANTY; without even the impliNed
 # 	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # 	GNU General Public License for more details. You should have received a
 # 	copy of the GNU General Public License along with this program. If not,
@@ -18,15 +18,52 @@
 
 use v5.12;
 use warnings;
+use POSIX;
+use Term::ProgressBar;
+use threads;
+use threads::shared;
+use Thread::Queue;
+use HTTP::Request::Common; 
 use Net::Amazon::S3;
+use Amazon::S3;
+
+# Fix bugs in Net::Amazon:S3
+*Net::Amazon::S3::Client::Object::put_part = sub {
+    my $self = shift;
+
+    my %args = ref($_[0]) ? %{$_[0]} : @_;
+
+    #work out content length header
+    $args{headers}->{'Content-Length'} = length $args{value}
+        if(defined $args{value});
+
+    my $response = $self->_perform_operation (
+        'Net::Amazon::S3::Operation::Object::Upload::Part',
+
+        upload_id   => $args{upload_id},
+        part_number => $args{part_number},
+        headers     => $args{headers},
+        value       => $args{value},
+    );
+
+    return $response->http_response;
+}; 
+
+*Net::Amazon::S3::Response::is_xml_content = sub {
+        my ($self) = @_;
+
+        #return $self->content_type =~ m:[/+]xml\b: && $self->decoded_content;
+        return $self->decoded_content && $self->decoded_content =~ /^\Q<?xml\E/;
+};
+ 
  
 # Load Configuration 
 use Config::Simple;
 use File::HomeDir;
 
 my $configurationFile;
-if (-e File::HomeDir->my_home . ".ftBackup") {
-	$configurationFile = File::HomeDir->my_home . ".ftBackup";
+if (-e File::HomeDir->my_home . "/.ftBackup") {
+	$configurationFile = File::HomeDir->my_home . "/.ftBackup";
 }
 elsif (-e '/etc/ftBackup') {
 	$configurationFile = "/etc/ftBackup";
@@ -60,15 +97,33 @@ say STDOUT 'Copyright (C) 2022 Universal Networks, LLC';
 say STDOUT 'By using this program the user acknowledges there is NO WARRANTY provided and';
 say STDOUT "accepts the terms of the software license, which is the GNU GPL 2.0 or later.\n";
 
-# create an S3 client
+# create our S3 clients
+# We use Net::Amazon::S3 for most operations since it supports copying.
+# However, its multipart support is buggy and we use Amazon::S3 for that.
+
 our $s3 = Net::Amazon::S3->new({
 		aws_access_key_id  => $config{'access_key_id'},
 		aws_secret_access_key => $config{'access_key'},
 		retry => 1
 	});
-
-# Create bucket object
+	
 our $s3Bucket = $s3->bucket($bucketName);
+	
+our $simpleS3 = Amazon::S3->new({
+		aws_access_key_id  => $config{'access_key_id'},
+		aws_secret_access_key => $config{'access_key'},
+		retry => 1
+	});	
+
+	# FIXME: Remove this.
+	#my $backupTarget = '/backup/2022-12-13/accounts/';
+	#my $target = 'asisaid';
+	#my @sortedDirectories = ( '2022-12-13' );
+
+	# Test code
+	
+	# exit;
+
 
 # Do today's backup
 # Directory grep based on https://stackoverflow.com/a/5751949/656780
@@ -85,11 +140,97 @@ closedir $backupDirectory;
 
 foreach my $target (@files) {
 	my $file = $backupTarget . $target;
+	my $archive = $backupTarget . $target . '.tar.gz';	
+	my $destinationObject = $backupPath . '/daily/' . $sortedDirectories[0] . '/' . $target . '.tar.gz';
+
 	print 'Backing up ' . $file . '...';
-	`tar -czf $file.tar.gz $file > /dev/null 2>&1`;
-	$s3Bucket->add_key_filename($file . '.tar.gz', $backupPath . '/daily/' . $sortedDirectories[0] . '/' . $target . '.tar.gz') or die "Failed to upload object: $!";
+	`tar -czf $archive $file > /dev/null 2>&1`;
+	
+	my $size = -s $archive;  
+	my $chunkSize = 500 * 1024 * 1024;
+	my $parts = ceil($size / $chunkSize);
+	my @queueData;
+	for (1...$parts) { push (@queueData, $_); }
+	my $overallProgress :shared;
+	my $queue = Thread::Queue->new(@queueData);
+
+	# Setup Progress Bar
+	my $progress = Term::ProgressBar->new({name  => $target,
+										   count => $parts,
+										   ETA   => 'linear', 
+										   remove => 1 });
+
+	# Initiate upload.
+	my $bucket = $simpleS3->bucket($bucketName); 
+	my $uploadId = $bucket->initiate_multipart_upload($destinationObject);
+	my %partList :shared;
+
+	say STDOUT "We need " . $parts . " parts for this upload." if ($config{'debug'});
+
+	my @threads;
+	for(1..4) {
+		push @threads, threads->create( sub {
+			# Open file handle for thread.
+			open(my $fileHandle, '<', $archive) or die("Error reading file, stopped");
+			binmode($fileHandle); 		
+			
+			# Pull work from the queue, don't wait if its empty
+			while( my $i = $queue->dequeue_nb ) {
+				my $chunk;
+				my $offset = ($i - 1) * $chunkSize;
+
+				seek ($fileHandle, $offset, 0);
+			
+				if ($chunkSize > $size) {
+					$chunkSize = $size;
+				}
+				elsif (($chunkSize + $offset) > $size) {
+					$chunkSize = $size - $offset;
+				}
+				
+				say STDERR "Chunk size for $i: " . $chunkSize . " starting at " . $offset if ($config{'debug'});
+
+				my $length = read($fileHandle, $chunk, $chunkSize);	
+
+				if ($config{'debug'}) {
+					say STDOUT "Chunk diff: " . (length($chunk) - $chunkSize);
+					say STDOUT "Requested size: " . $chunkSize;	
+					say STDOUT "Received size: " . length($chunk);
+					say STDOUT "Stated size: " . $length;
+				}
+
+				my $bucketStep = $simpleS3->bucket($bucketName); 
+				$partList{$i} = $bucketStep->upload_part_of_multipart_upload($destinationObject, $uploadId, $i, $chunk);
+
+				unless ($partList{$i}) {
+					say STDERR "Going to need to repeat " . $i . ".";
+					$queue->enqueue($i);
+					next;
+				}		
+
+				#Advance Progress Bar
+				$overallProgress++;			
+				$progress->update($overallProgress);
+				$progress->message( 'Number: ' . $i);
+			}
+			
+			# Close file handle for thread.
+			close($fileHandle); 
+	   });
+	}
+
+	# Wait for threads to finish
+	$_->join for @threads;
+
+
+	my $result = $bucket->complete_multipart_upload($destinationObject, $uploadId, \%partList);	
+	
+	unless ($result) {
+		die 'Failed to upload ' . $target . ' backup';
+	}
+
 	print "Done.\n";
-	unlink $file . '.tar.gz';
+	unlink $archive;
 }
 
 # Cull old backups
@@ -199,5 +340,7 @@ sub doesBackupExist {
 	
 	return (!@{ $targetCheck->{'keys'} }) ? 0 : 1;
 }
+
+
 
 1;
