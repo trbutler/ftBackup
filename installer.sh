660a3672809997e6ed9c5de6f14b8adf14fa24dc
timothy butler <timothybutler@timothys-MacBook-Pro.local>
2022-12-18 00:22:36 -0600

    Improvements to installer.

--- installer.sh
+++ installer.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-echo "Installing FaithTree::Backup..."
+echo "Checking for non-core required Perl modules...";
+cpan Net::Amazon::S3
+cpan Term::ProgressBar
+echo "\n\nInstalling FaithTree::Backup...";
 ./Makefile.PL
 make
 make install
-echo "Don't forget to edit ftBackup.conf and place it in an appropriate location (see README.md)";
+echo "\n\nDon't forget to edit ftBackup.conf and place it in an appropriate location (see README.md)";
