4fb731f4197cd387320c2fba305c5d5eb2752a92
timothy butler <timothybutler@timothys-MacBook-Pro.local>
2022-12-17 23:51:06 -0600

    Updated doc on installation; finished conversion to Makefile system.

--- installer.sh
+++ installer.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 echo "Installing FaithTree::Backup..."
-mkdir /var/cpanel/perl5/lib/FaithTree
-mkdir /var/cpanel/perl5/lib/FaithTree/Backup
-cp FaithTree/Backup.pm /var/cpanel/perl5/lib/FaithTree/
-cp FaithTree/Backup/Logger.pm /var/cpanel/perl5/lib/FaithTree/Backup/
-cp ftBackup.pl /var/cpanel/perl5/lib/
-ln -s /var/cpanel/perl5/lib/ftBackup.pl /usr/local/bin/ftBackup 
-chmod 755 /var/cpanel/perl5/lib/ftBackup.pl
-cpan Net::Amazon::S3
-cpan Term::ProgressBar
-/usr/local/cpanel/bin/manage_hooks add script /var/cpanel/perl5/lib/ftBackup.pl --manual --category=System --event=Backup --stage=post
- 
+./Makefile.PL
+make
+make install
